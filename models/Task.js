abba48c39ea54259aa881e3dfe5cf4e19db3f7d5
Aks <akspatel225@gmail.com>
2023-03-30 22:28:16 +0530

    initial commmit

---
+++ models/Task.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const TaskSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Please provide title"],
+      maxlength: 50,
+    },
+    completed: {
+      type: Boolean,
+      default: false,
+    },
+    createdBy: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: [true, "Please provide user"],
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Task", TaskSchema);
